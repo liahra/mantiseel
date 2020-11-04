0980556d8cc753f4c5c4ecd9d363a622516766f0
espenraugstad <espen.raugstad@gmail.com>
2020-11-04 12:41:03 +0100

    ÂMidt i loginstress

--- server.js
+++ server.js
@@ -15,30 +15,29 @@ const db = new storage(credentials);
 /* MIDDLEWARE - MOVE TO SEPARATE MODULES ETC */
 
 const authenticator = async (req, res, next) => {
-
+    console.log('Authenticating....');
     //If no authorization header:
     if(!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1){
         return res.append("WWW-Authenticate", 'Basic realm="User Visible Realm", charset="UTF-8"').status(401).end();
     } else {
-        //Else - check password in database etc.
-        console.log(req.body);
-
-        let username = req.body.username;
-        let password = req.body.password;
+        //GET THIS INFO FROM HEADER!
+        const credentials = req.headers.authorization.split(' ')[1];
+        const [username, password] = Buffer.from(credentials, 'base64').toString('UTF-8').split(":");
 
         //Retrieve username and password from database
-        let user = await db.getUser(username);
-        console.log(user);
-        let passwordFromDB = user.password;
+        let user, passwordFromDB;
+        if(username){
+            user = await db.getUser(username);
+            passwordFromDB = user.password;
+        }
         
         //If password OK - login = ok
         if(encrypt.comparePasswords(encrypt.encryptPassword(password), passwordFromDB)){
-            res.status(200);
+            res.send('ok');
         } else {
-            res.status(403);
+            //res.status(403);
+            res.send('no');
         }
-
-        //Else respond 403
     }
 
     
@@ -46,9 +45,10 @@ const authenticator = async (req, res, next) => {
 }
 
 /* ****************************************** */
-
-
-
+server.post('/logout', async(req, res)=>{
+    console.log(req.headers);
+    res.status(403).end();
+});
 
 server.post('/api/makePresentation', async (req, res) => {
  
@@ -109,22 +109,31 @@ server.post('/api/deleteUser', async (req, res) => {
 });
 
 server.post('/api/updateUser', async (req, res) => {
- 
+
 
     res.status(200).json(response).end();
-});
-
-//post eller get?
-server.post('/api/login', authenticator, async (req, res) => {
- 
-    res.send('Secret message');
-    //res.status(200).json(response).end();
 });
 
 server.post('/api/sharePresentation', async (req, res) => {
  
 
     res.status(200).json(response).end();
+});
+
+/* ALL ENDPOINTS THAT REQUIRE AUTHENTICATION */
+server.use(authenticator);
+
+//post eller get?
+server.post('/api/login', async (req, res) => {
+    console.log(res);
+    //res.status(200).end();
+    //res.status(200).json(response).end();
+});
+
+server.get('/random', async (req, res)=>{
+    console.log('Random 1');
+    res.sendFile(__dirname + '/logins/random.html');
+    console.log('Random 2');
 });
 
 server.set('port', (process.env.PORT || 8080));
